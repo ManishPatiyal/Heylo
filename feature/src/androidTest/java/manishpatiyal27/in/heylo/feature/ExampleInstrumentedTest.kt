02b8b05a1d413bde4f851defc466eca1ab9ac1b7
manish.patiyal@vtnetzwelt.com <manish.patiyal@vtnetzwelt.com>
2018-03-29 16:40:04 +0530

    Initial Commit

---
+++ feature/src/androidTest/java/manishpatiyal27/in/heylo/feature/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package manishpatiyal27.`in`.heylo.feature
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("manishpatiyal27.in.heylo.feature.test", appContext.packageName)
+    }
+}
